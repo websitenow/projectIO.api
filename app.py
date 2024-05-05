d1837b89c41425389d3cf4549f09a3fae3355ed5
Developer <123030444+websitenow@users.noreply.github.com>
2024-05-05 14:11:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from requests import get
 
 app = Flask(__name__)
 
-@app.route("/")
-def homepage():
-    return "Hello A<span style='color: red;'>PY</span>"
+GOOGLEURL = "https://www.google.com/search?tbm=isch&q="
+
+@app.route("/<name>")
+def homepage(name):
+    try:
+        html = get(GOOGLEURL + name)
+        return html.text.replace('"/', '"https://www.google.com/')
+    except:
+        return ":( <span style='color: red;'>Error</span>"
 
 if __name__ == "__main__":
     app.run()
