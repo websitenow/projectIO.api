4143fe3f234fc392a3473657f5fa01fac676f196
Developer <123030444+websitenow@users.noreply.github.com>
2024-05-05 14:30:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ app = Flask(__name__)
 
 GOOGLEURL = "https://www.google.com/search?tbm=isch&q="
 
+@app.route("/")
+def home():
+    return "Hello!!!"
+
 @app.route("/<name>")
-def homepage(name):
+def search(name):
     try:
         html = get(GOOGLEURL + name)
         return html.text.replace('"/', '"https://www.google.com/')
