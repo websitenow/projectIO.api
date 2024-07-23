ca64132503c66dbca06ea98553dce761c2998ab6
Developer <123030444+websitenow@users.noreply.github.com>
2024-07-22 22:48:55 -0300

    update app.py

--- app.py
+++ app.py
@@ -1,23 +1,40 @@
-from flask import Flask
-from requests import get
-from os import environ
+import requests
+from re import findall, compile
+from flask import Flask, request
+from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
 
-CONST = ['URL_1', 'URL_2']
-
-@app.route("/")
-def home():
-    return "Hello!!!"
-
-@app.route("/<index>/<name>")
-def search(index, name):
+def get_captions(url):
     try:
-        url = environ[CONST[int(index)]]
-        html = get(url + name)
-        return html.text
+        r = requests.get(url,timeout=5)
+        if r.status_code == 200:
+            html = r.text
+            pdr = r'"captionTracks":\[\{(.*?)\}\]'
+            pdr2 = r'http[a-z]:\/\/(.*?)\"'
+            founds = findall(pdr, html)
+            if len(founds) == 0:
+                return
+           
+            founds = ['[{' + x.replace('\\u0026', '&') + '}]' for x in founds]
+            
+            return founds
     except:
-        return ":( <span style='color: red;'>Error</span>"
+        pass
+        
+@app.route('/')
+def GETcaptions():
+    url = request.args.get('url')
+    
+    if url:
+        captions = get_captions(url)
+        captions = captions if captions else []
+        return captions
+    return []
+
+@app.errorhandler(HTTPException)
+def handle_bad_request(error):
+    return f'bad request! {error.code}'
 
 if __name__ == "__main__":
     app.run()
