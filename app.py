ff91782556cfe46c25f01e28455d48e3c5590659
Developer <123030444+websitenow@users.noreply.github.com>
2024-09-17 17:01:34 +0000

    WATH

--- app.py
+++ app.py
@@ -1,49 +1,20 @@
 import requests
 from re import findall, compile
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from werkzeug.exceptions import HTTPException
 from json import loads, dumps
+from os import environ
 
 app = Flask(__name__)
 
-def get_captions(url):
-   try:
-       r = requests.get(url,timeout=5)
-       if r.status_code == 200:
-           html = r.text
-           pdr = r'"captionTracks":\[\{(.*?)\}\]'
-           pdr2 = r'http[a-z]:\/\/(.*?)\"'
-           founds = findall(pdr, html)
-           if len(founds) == 0:
-               return
-           res = ""
-           for data in founds:
-               res += '[{' + data.replace('\\u0026', '&').replace("'",'"') + '}]'
-           return res
-   except:
-       pass
-        
-@app.route('/')
-def GETcaptions():
-   url = request.args.get('url')
-    
-   if url:
-       captions = get_captions(url)
-       captions = captions if captions else []
-       return captions
-   return []
+@app.route("/")
+def homepage():
+    return "<h2>HOMEPAGE</h2><br><a href='/proxy'>"
 
-@app.route('/url')
-def getUrl():
-   url = request.args.get('url')
-   if url:
-       try:
-           url = "https://" + url
-           html = requests.get(url)
-           return html.text
-       except Exception as e:
-           return f"{e}{url}"
-   return f"{url}"
+@app.route('/proxy')
+def proxy():
+   actived = environ.get("PROXYE_ACTIVED")
+   return render_template("uploadproxye.html", actived=actived)
 
 
 @app.errorhandler(HTTPException)
